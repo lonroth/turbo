5916a1dfade1236c02d84500cb1a30316a55e6f7
Kirill Platonov <mail@kirillplatonov.com>
2021-04-14 12:20:00 +0300

    Add dist folder

---
+++ dist/types/core/request_interceptor.d.ts
@@ -0,0 +1,5 @@
+export declare class RequestInterceptor {
+    static interceptor?: (request: any) => void;
+    static register(interceptor: (request: any) => void): void;
+    static get(): ((request: any) => void) | undefined;
+}
